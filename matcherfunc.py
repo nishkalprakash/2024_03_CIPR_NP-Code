7738e06b697b5f05e8926eaef9ba7b808ad931d1
Abhibhu <prakashabhibhu@gmail.com>
2023-12-06 15:37:56 +0530

    changes in score calculator

--- matcherfunc.py
+++ matcherfunc.py
@@ -94,7 +94,7 @@ def find_match(array, index, threshold1, threshold2):
 
     def flag_calculator(element1, element2, threshold1, threshold2):
         # score = 1 - (sqrt((e1[1]-e2[1])^2)/t1 - sqrt((e1[2]-e2[2])^2 + (e1[3]-e2[3])^2 + (e1[4]-e2[4])^2)/sqrt(3)*t2) /2
-        return 1 - ((np.sqrt((element1[1]-element2[1])**2)/threshold1) * 1/np.sqrt(1 + abs(element1[1]) + abs(element2[1])) + np.sqrt((element1[2]-element2[2])**2 + (element1[3]-element2[3])**2 + (element1[4]-element2[4])**2)/1.7321*threshold2) /2
+        return 1 - ((np.sqrt((element1[1]-element2[1])**2)/threshold1) * 1/(1 + abs(element1[1]) + abs(element2[1]))**0.2 + np.sqrt((element1[2]-element2[2])**2 + (element1[3]-element2[3])**2 + (element1[4]-element2[4])**2)/1.7321*threshold2) /2
 
     def match_function(element1, element2, threshold1, threshold2):
         if abs(element1[1]-element2[1]) < threshold1:
